c0a67fcdbd0ab2c1024e2ddebd9c4ba0a8e64a09
Carlos Alberto Souza Nascimento <carlosalbertosn.10@gmail.com>
2024-07-09 18:27:34 -0300

    implementando a execuacao das funcionaldiades da Feature Produto no commandLinneRunner

--- src/main/java/br/com/compassuol/Desafio/_3/Desafio03Application.java
+++ src/main/java/br/com/compassuol/Desafio/_3/Desafio03Application.java
@@ -1,13 +1,25 @@
 package br.com.compassuol.Desafio._3;
 
+import br.com.compassuol.Desafio._3.repository.ProdutoRepository;
+import br.com.compassuol.Desafio._3.view.ProdutoView;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Desafio03Application {
+public class Desafio03Application implements CommandLineRunner {
+
+	@Autowired
+	private ProdutoRepository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Desafio03Application.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		ProdutoView produtoView = new ProdutoView(produtoRepository);
+		produtoView.exibirMenu();
+	}
 }
